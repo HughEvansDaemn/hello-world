9d9b3d741a17607fb87b9afc6d2e97ad85e0a15e
HughEvansDaemn <hugh.evans@dae.mn>
2022-01-26 10:48:24 +0000

    Created Rest API with Spring

---
+++ src/main/java/org/example/rest/HelloWorld.java
@@ -0,0 +1,16 @@
+package org.example.rest;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class HelloWorld {
+    @GetMapping(value="/greeting",produces=MediaType.TEXT_PLAIN_VALUE)
+    public String greeting() {
+        return "Hello World";
+    }
+
+}
